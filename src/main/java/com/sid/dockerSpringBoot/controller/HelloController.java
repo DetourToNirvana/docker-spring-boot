b0edc9ac03aaf68f4ee2aeb6b068b201dda49f05
DetourToNirvana <siddharth.indurthi@gmail.com>
2020-02-13 01:57:18 +0530

    simplify base url

--- src/main/java/com/sid/dockerSpringBoot/controller/HelloController.java
+++ src/main/java/com/sid/dockerSpringBoot/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("docker-java-app")
+@RequestMapping("app")
 public class HelloController {
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
